2bf5f24134420505c9142aa1a2a83ed4f01c9e44
joao.ferreira <joao.ferreira@corp.badoo.com>
2018-07-12 16:29:39 +0100

    Better keybindings over defaults

--- modules/keybinding.el
+++ modules/keybinding.el
@@ -15,37 +15,45 @@
   :init
   :config
   (general-define-key
-   :prefix "C-,"
+   :prefix "C-c"
    "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
    "SPC" '(helm-M-x :which-key "M-x")
 
+   "!" '(:which-key "flycheck")
+   "$" '(:which-key "correct word")
+
    "e" '(:which-key "checks")
    "el" '(flycheck-list-errors :which-key "list errors")
-   
+
    "g" '(:which-key "git")
-   "gs" '(magit-status :which-key "magit status")
+   "gg" '(magit :which-key "magit")
    "gb" '(magit-blame :which-key "magit blame")
    "gh" '(git-timemachine :which-key "history")
    
    "f" '(:which-key "files")
    "ff" '(helm-find-files :which-key "find file")
    "fk" '(delete-file :which-key "delete file")
-   "fs" '(helm-ag :which-key "find occurrences")
    
    "p" '(:which-key "project")
-   "pf" '(helm-projectile-find-file :which-key "find files")
-   "ps" '(helm-projectile-ag :which-key "find occurrences")
-   "pl" '(helm-projectile-switch-project :which-key "choose project")
+   "pf" '(helm-projectile-find-file)
+   ;;"ps" '(helm-projectile-ag :which-key "find occurrences")
+   "pp" '(helm-projectile-switch-project)
+   "ps" '(:which-key "search")
+   "px" '(:which-key "shell")
+	  
    
    "b" '(:which-key "buffers")
+   "b]" '(next-buffer :which-key "next buffer")
+   "b[" '(previous-buffer :which-key "previous buffer")
    "bk" '(kill-buffer :which-key "kill buffer")
    "bb" '(helm-buffers-list :which-key "buffers list")
    "be" '(eval-buffer :which-key "eval buffer")
 
-   "a" '(:which-key "apps")
-   "ar" '(ranger :which-key "ranger")
+   "r" 'ranger
    
    "w" '(:which-key "window")
+   "w]" '(next-multiframe-window :which-key "next window")
+   "w[" '(previous-multiframe-window :which-key "previous window")
    "w/" '(split-window-right :which-key "split right")
    "w-" '(split-window-below :which-key "split bottom")
    "wd" '(delete-window :which-key "delete window")
