9df97bb80da4bdee7140aff118333e3a9cc96742
joao.ferreira <joao.ferreira@corp.badoo.com>
2018-08-19 21:40:52 +0100

    Adding companyu-jedi for python

--- lang/lang-python.el
+++ lang/lang-python.el
@@ -1,10 +1,22 @@
-;;; python
 
+;;; python
+      
 (use-package blacken
   :ensure t
+  :after (python)
   :init
-  :bind (("C-c C-b" . blacken-buffer))
+  :bind (:map python-mode-map ("C-c C-b" . blacken-buffer))
   )
+
+(use-package company-jedi
+  :ensure t
+  :after (company python)
+  :init
+  (defun my/python-mode-hook ()
+    (add-to-list 'company-backends 'company-jedi))
+  (add-hook 'python-mode-hook 'my/python-mode-hook)
+  :bind (:map python-mode-map ("M-." . 'jedi:goto-definition)))
+  
 
 (provide 'lang-python)
 ;;; python.el ends here
