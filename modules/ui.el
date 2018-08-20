a4fe562ef2713e7f1dd344d24c91ea682429c56f
joao.ferreira <joao.ferreira@corp.badoo.com>
2018-08-20 10:55:39 +0100

    Removing ace-window

--- modules/ui.el
+++ modules/ui.el
@@ -41,13 +41,6 @@
 (setq ns-use-proxy-icon nil)
 (setq frame-title-format "%b")
 
-(use-package ace-window
-  :ensure t
-  :init
-  (setq aw-scope 'frame)
-  :config
-  (ace-window-display-mode t))
-
 ;; Theme
 (use-package doom-themes
   :ensure t
