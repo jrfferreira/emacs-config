7d471a0f22bfa0082ebb50e00804871b872b5f07
joao <joao.ferreira@corp.badoo.com>
2018-12-06 17:57:23 +0000

    Improving projectile search configs

--- modules/core.el
+++ modules/core.el
@@ -1,3 +1,4 @@
+
 ;;; Core modules
 
 (use-package undo-tree
@@ -16,19 +17,19 @@
   :ensure t
   :init
   (setq helm-M-x-fuzzy-match t
-  helm-mode-fuzzy-match t
-  helm-buffers-fuzzy-matching t
-  helm-recentf-fuzzy-match t
-  helm-locate-fuzzy-match t
-  helm-semantic-fuzzy-match t
-  helm-imenu-fuzzy-match t
-  helm-completion-in-region-fuzzy-match t
-  helm-candidate-number-list 150
-  helm-split-window-in-side-p t
-  helm-move-to-line-cycle-in-source t
-  helm-echo-input-in-header-line t
-  helm-autoresize-max-height 0
-  helm-autoresize-min-height 20)
+	helm-mode-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match t
+	helm-locate-fuzzy-match t
+	helm-semantic-fuzzy-match t
+	helm-imenu-fuzzy-match t
+	helm-completion-in-region-fuzzy-match t
+	helm-candidate-number-list 150
+	helm-split-window-in-side-p t
+	helm-move-to-line-cycle-in-source t
+	helm-echo-input-in-header-line t
+	helm-autoresize-max-height 0
+	helm-autoresize-min-height 20)
   :config
   (helm-mode 1)
   :bind (("C-x C-f" . #'helm-find-files)
@@ -43,6 +44,10 @@
 (use-package helm-projectile
   :ensure t
   :after (helm)
+  :init
+  (setq helm-projectile-fuzzy-match t
+	projectile-enable-caching t
+	projectile-switch-project-action 'helm-projectile-find-file)
   :config
   (helm-projectile-on))
 
